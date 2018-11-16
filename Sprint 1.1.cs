a10371eb51d1c85e77981de7d65581194bd79808
Stijn456 <44999891+Stijn456@users.noreply.github.com>
2018-11-16 13:43:49 +0100

    Sprint 1.1

---
+++ Sprint 1.1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Naam: Stijn Lassche");
+            Console.WriteLine("Straat: Topaasstraat 16");
+            Console.WriteLine("Adress: 7548CL");
+            Console.WriteLine("Leeftijd: 17");
+            Console.ReadLine();
+            
+
+        }
+    }
+}
